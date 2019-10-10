df1c9924ad5e9ceda5a2e3f6fefa875e7ad0d29d
Thiago Yasunaka <thiagoyasunaka@hotmail.com>
2019-10-10 08:56:27 -0300

    bfs quase finalizado

--- diametro.py
+++ diametro.py
@@ -1,6 +1,37 @@
+BRANCO = "branco"
+CINZA = "cinza"
+PRETO = "preto"
+
 def bfs(G, s):
-    for vertex in G:
-        
+    initialState = {
+        "d": float("inf"),
+        "pi": None,
+        "cor":BRANCO,
+        "adj": []
+    }
+    GAux = G
+    for index,vertex in enumerate(G):
+        adj = vertex
+        vertex = initialState
+        vertex['adj'] = adj
+        GAux[index] = vertex
+    GAux[s]['d'] = 0
+    GAux[s]['pi'] = None
+    GAux[s]['cor'] = CINZA
+    q = [] #Pilha Vazia
+    q.append(GAux[s])
+    while q != []:
+        u = q.pop()
+        for v in u['adj']:
+            if GAux[v]['cor'] == BRANCO:
+                GAux[v]['d'] = u['d'] + 1
+                GAux[v]['pai'] = u #GUARDAR SOMENTE O ENDEREÇO DO PAI E NÃO O OBJETO INTEIRO
+                GAux[v][cor] = CINZA
+                q.append(GAux[v])
+        u['cor'] = PRETO #ARRUMAR, NÃO DEVEMOS SETAR O VALOR DO u E SIM GAux[v]
+
+    print(GAux)
+
 
 
 def teste_bfs():
@@ -20,9 +51,18 @@ def teste_bfs():
         [r],
         [s,t,x],
         [w,t,u,y],
-        [u,x]]
-    
-    d = bfs(G, G[0])
-    assert d[s] == 0
+        [u,x]
+    ]
+    bfs(G, 1)
+    #d = bfs(G, G[1])
+    #assert d[r] == 1
+    #assert d[s] == 0
+    #assert d[t] == 2
+    #assert d[u] == 3
+    #assert d[v] == 2
+    #assert d[w] == 1
+    #assert d[x] == 2
+    #assert d[y] == 3
 
+    
 teste_bfs()
