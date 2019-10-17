123c217a354e4f78d6bfd2685028c90686c2b7eb
otavio <otaviokochi@gmail.com>
2019-10-16 21:21:10 -0300

    func eh arvore

--- diametro.js
+++ diametro.js
@@ -3,6 +3,25 @@ const BRANCO = 'branco'
 const CINZA = 'cinza'
 const PRETO = 'preto'
 
+
+let r = 0
+let s = 1
+let t = 2
+let u = 3 
+let v = 4
+let w = 5
+let x = 6
+let y = 7
+
+let a = 0
+let b = 1
+let c = 2
+let d = 3
+let e = 4
+let f = 5
+let g = 6
+let h = 7
+
 const diametro = (G) => {
     const s = Math.round(Math.random() * (G.length - 1))
     const a = bfs(G, s).reduce((number1, number2) => {
@@ -108,7 +127,7 @@ const teste_bfs = () => {
         [t]                     //     /
     ]                           //    w
     let d = bfs(G, 1)
-    let d1 = bfs(G2, 0)    
+    let d1 = bfs(G2, 0)
 
     assert (d[r] == 1)
     assert (d[s] == 0)
@@ -128,5 +147,73 @@ const teste_bfs = () => {
 
 }
 
+const numero_arestas = (G) => {
+    let contador = 0;
+    if(G.length > 1)
+        for(let i = 0; i < G.length; i ++){
+            for(let j = 0; j < G[i].length; j ++){
+                if(G[i][j] <= i)
+                    contador++;
+            }
+        }
+    return contador;
+}
+
+const teste_aresta = () => {
+    const G4 = [
+        [b,c],                     //       a
+        [a],                       //     /   \  
+        [a]                        //   b      c
+    ]
+    const G = [
+        [a]
+    ]
+    const G2 = [
+        [s,v],                  //      r
+        [r,t,u],                //     / \
+        [w,s],                  //    v   s
+        [s],                    //       / \
+        [r],                    //      t   u
+        [t]                     //     /
+    ]                           //    w
+    const G3 = [
+        [b,c],                      //          a
+        [a,e,d],                    //         /  \
+        [a,f],                      //        b    c
+        [b],                        //       / \     \
+        [b],                        //      e   d     f
+        [c,g],                      //                 \ 
+        [f],                        //                  g
+    ]                                           
+    const are1 = numero_arestas(G)
+    const are2 = numero_arestas(G2)
+    const are3 = numero_arestas(G3)
+    const are4 = numero_arestas(G4)
+    assert(are1 == 0)
+    assert(are2 == 5)
+    assert(are3 == 6)
+    assert(are4 == 2)
+}
+
+const random_tree_random_walk = (n) => {
+
+}
+
+const eh_arvore = (G) => {
+    const arestas = numero_arestas(G)
+    if(arestas != (G.length - 1)){
+        return false
+    }
+    const s = Math.floor(Math.random() * (G.length - 1))
+    const aux = bfs(G, s)
+    for(let i = 0; i < G.length; i ++){
+        if(aux[i] === Number.POSITIVE_INFINITY)
+            return false
+    }
+    return true
+}
+
+
 teste_bfs()
 teste_diametro()
+teste_aresta()
